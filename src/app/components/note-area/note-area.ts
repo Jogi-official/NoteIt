bcf0c951cc33e16cc7ff102205ef37b7e3ad292d
Akash Joshi <akashjoshirm.aj@gmail.com>
2026-02-19 21:37:51 +0530

    Added add notes section

--- src/app/components/note-area/note-area.ts
+++ src/app/components/note-area/note-area.ts
@@ -1,9 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Tasks } from './components/tasks/tasks';
 
 @Component({
   selector: 'app-note-area',
-  imports: [],
+  imports: [CommonModule, Tasks],
   templateUrl: './note-area.html',
   styleUrl: './note-area.css',
 })
-export class NoteArea {}
+export class NoteArea {
+  currentDateTime: Date = new Date();
+
+  ngOnInit() {
+    
+    setInterval(() => {this.currentDateTime = new Date()}, 1000);
+  }
+
+}
