6833794f2dab04002c359d6f3555026da14b61c8
Akash Joshi <akashjoshirm.aj@gmail.com>
2026-02-19 09:40:28 +0530

    added my projects section with scrollbar

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NotificationArea } from './components/notification-area/notification-area';
 import { NotesControl } from './components/notes-control/notes-control';
+import { MyProjects } from './components/my-projects/my-projects';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NotificationArea, NotesControl],
+  imports: [NotificationArea, NotesControl, MyProjects],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
